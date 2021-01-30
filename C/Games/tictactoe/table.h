d15f1952774fa1d681abb924af32c7e830f23ff7
Muzaffer <muzafferirmaky@gmail.com>
2021-01-30 20:03:32 +0300

    Game Reseting

--- C/Games/tictactoe/table.h
+++ C/Games/tictactoe/table.h
@@ -1,28 +1,7 @@
 #if !defined TABLE_H
 #define TABLE_H
-#include "pieces.h"
-#include <stdio.h>
-
-#define TABLE_ROW_SIZE 5
-#define TABLE_COLUMN_SIZE 10
-
-char gameTable[TABLE_ROW_SIZE][TABLE_COLUMN_SIZE];
 
 
-void table_init();
-/*
-                     :   :
-                  ---:---:---
-                     :   :
-                  ---:---:---
-                     :   :
-*/
-
-void table_print();
-
-  //TODO add move 
-
-//void add_move_on_table(Pieces piece , Move move);
 
 
 
