c70c50768eeb0ffc68254617adcc6bad3f113cd1
Muzaffer <muzafferirmaky@gmail.com>
2021-02-03 15:40:38 +0300

    commit3

---
+++ C/Chess/headers/analyze.h
@@ -0,0 +1,14 @@
+#ifndef ANALYZE_H
+#define ANALYZE_H
+
+#include <string.h>
+
+// get raw string from game
+// split it into notation and game info
+
+
+
+
+void split_string(char *string);
+
+#endif
---
+++ C/Chess/headers/game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include "analyze.h"
+#include "move.h"
+#include "table.h"
+
+
+
+void game_loop();
+
+
+#endif
